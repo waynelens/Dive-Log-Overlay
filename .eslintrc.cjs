9da2963dfa321f0c1983b6589a16aa5496377a1d
WayneJin <hi@waynelens.dev>
2025-10-25 12:49:47 +0800

    chore: add ESLint and Prettier configuration for code formatting

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // Vue specific rules
+    'vue/multi-word-component-names': 'off',
+    'vue/no-v-html': 'warn',
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'warn',
+    
+    // General rules
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+  vueIndentScriptAndStyle: false,
+}
